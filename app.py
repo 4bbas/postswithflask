03601e5b23fc7501904cf3bef4ffdcd263a5ee5e
@bb@s <abbas_sheikh@yahoo.com>
2024-03-23 15:45:25 +0000

    added template and variable

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+POSTS = [{
+    "Id": 1,
+    "Title": "My first post",
+    "Body": "This is my first post",
+    "Slug": "my-first-post"
+}, {
+    "Id": 2,
+    "Title": "My second post",
+    "Body": "This is my second post",
+    "Slug": "my-second-post"
+}]
 
 
 @app.route('/')
 def Index():
-  return "Hello World"
+  return render_template("home.html", posts=POSTS)
+
+
+@app.route('/api/posts')
+def list_jobs():
+  return jsonify(POSTS)
 
 
 if __name__ == "__main__":
